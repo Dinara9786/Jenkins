5f82e0e5d47c480abdfa3c595db633fc3a696d4a
Dinara9786 <knowledge9786@gmail.com>
2021-03-06 11:29:35 -0500

    another commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,14 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+
+        for (int i=0; i<5; i++) {
+            System.out.println("Hello Batch 8");
+
+            Thread.sleep(2000);
+        }
+    }
+}
